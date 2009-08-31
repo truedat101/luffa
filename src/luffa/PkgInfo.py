ff7a70effacb35ac440df32ecca718f9846b1a76
David J. Kordsmeier <dkords@torino.local>
2009-08-31 16:56:20 -0700

    Fix for merge between v 0.2 and 0.18.

--- src/luffa/PkgInfo.py
+++ src/luffa/PkgInfo.py
@@ -1,9 +1,5 @@
 package = "luffa"
-<<<<<<< HEAD:src/luffa/PkgInfo.py
 version = "0.2"
-=======
-version = "0.118"
->>>>>>> 6d9895354a8a3f5fac20315a0514d21a21f7f102:src/luffa/PkgInfo.py
 url = "http://github.com/truedat101/luffa/tree/master"
 license = "BSD License"
 short_description = "luffa is a small tool for identifying potential legal encumbrances in proprietary projects planning an open source release."
